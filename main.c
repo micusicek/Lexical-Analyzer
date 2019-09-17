35d18faad95a904d6c9d069b6c76fe02c382dcdf
joaocprado <jopradoxd@gmail.com>
2019-09-17 11:40:31 -0700

    add for-loop, will be used to find occurrencies of symbols in the tokens

--- main.c
+++ main.c
@@ -3,11 +3,18 @@
 
 int main() {
     char str[] = {" { int b; b = 1; { int a; a = 2; do a = a+1; while (a < 100); }"};
+    char temp[12];
 
     char *tkn;
     tkn = strtok (str," ");
     while (tkn != NULL) {
         printf ("%s", tkn);
+
+        strcpy(temp, tkn);
+        for (int i = 0; i < strlen(temp); i++)
+        {
+            //find ocurrencies
+        }
         
         if(!strcmp(tkn, "int")){
             printf(" - BASE_TYPE");
@@ -21,19 +28,9 @@ int main() {
             printf(" - WHILE");
         }
 
-        
-
-
-
-
-
-        
-        
-    
         tkn = strtok (NULL, " ");
         printf("\n");
     }
-
 
     return 0;
 }
