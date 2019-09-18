9b45ac50c494672089a71416c6e819f03adb0879
Alexandra Krutekova <32754153+micusicek@users.noreply.github.com>
2019-09-18 10:59:03 -0700

    Update main.c

--- main.c
+++ main.c
@@ -3,34 +3,13 @@
 
 int main() {
     char str[] = {" { int b; b = 1; { int a; a = 2; do a = a+1; while (a < 100); }"};
-    char temp[12];
-
-    char *tkn;
-    tkn = strtok (str," ");
-    while (tkn != NULL) {
-        printf ("%s", tkn);
-
-        strcpy(temp, tkn);
-        for (int i = 0; i < strlen(temp); i++)
-        {
-            //find ocurrencies
-        }
-        
-        if(!strcmp(tkn, "int")){
-            printf(" - BASE_TYPE");
-        }
-
-        if(!strcmp(tkn, "do")){
-            printf(" - DO");
-        }
-
-        if(!strcmp(tkn, "while")){
-            printf(" - WHILE");
-        }
-
-        tkn = strtok (NULL, " ");
-        printf("\n");
-    }
-
+    //read string str character by character
+    //if delimiter process current token
+    //if not delimiter add current cahracter to currnet token
+    //
+    //processing token
+    //find which token it is 1-31 from token table
+    //print it
+    //if it's not a valid token print error
     return 0;
 }
